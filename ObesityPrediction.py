f8a7f9a5503eddfcd1bc1cf206e500bf32252260
Coded Coder <105555015+fredydevelop@users.noreply.github.com>
2026-04-22 06:47:27 +0100

    Update ObesityPrediction.py

--- ObesityPrediction.py
+++ ObesityPrediction.py
@@ -366,9 +366,9 @@ def multi(input_data):
         dfresult = pd.DataFrame({
             "User_ID": np.arange(len(prediction)),
             "Category": [r[0] for r in recommendations_split],
-            "Recommendation 1": [r[1] for r in recommendations_split],
-            "Recommendation 2": [r[2] for r in recommendations_split],
-            "Recommendation 3": [r[3] for r in recommendations_split],
+            "Recommendation 1": [r[1] for r in recommendations],
+            "Recommendation 2": [r[2] for r in recommendations],
+            "Recommendation 3": [r[3] for r in recommendations],
         })
         
 
